e7128ca2c534f6dc13f7e2bb13da08f411a0a754
kamuirokuro <79147759+kamuirokuro@users.noreply.github.com>
2021-02-26 02:57:39 +0700

    Add files via upload

---
+++ start-mining-combode.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --algorithm phi5 --pool eu.poolmine.xyz:8334 --wallet CeJg7p4vDameTtsocAhbGeKArwnwv89Jhz --password c=CBE
